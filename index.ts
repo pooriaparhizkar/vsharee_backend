8036ae2547f5c624e1973fbdc9aa6e8c9b97948d
pooriaparhizkar <pooriaparhizkar@yahoo.com>
2024-03-18 01:27:11 +0330

    Add: websocket

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+// index.ts
+
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -7,6 +9,8 @@ import userRoutes from './routes/user';
 import groupRoutes from './routes/group';
 import swaggerUi from 'swagger-ui-express';
 import specs from './swaggerConfig';
+import http from 'http'; // Import http module for WebSocket
+import WebSocket from 'ws'; // Import WebSocket module
 
 const app = express();
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs));
@@ -16,20 +20,48 @@ app.use(cors());
 app.use(bodyParser.json());
 require('dotenv').config();
 
-
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI ?? "")
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
-
 // Define Routes
-// Example:
-// app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/group', groupRoutes);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Create HTTP server
+const server = http.createServer(app);
+
+// Create WebSocket server and attach it to HTTP server
+const wss = new WebSocket.Server({ server });
+
+// WebSocket connection event handler
+wss.on('connection', function connection(ws) {
+
+    // WebSocket message event handler
+    ws.on('message', function incoming(message) {
+
+        // Convert message to string if it's not already
+        const stringMessage = typeof message !== 'string' ? message.toString() : message;
+
+        // Broadcast message to all connected WebSocket clients
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(stringMessage);
+            }
+        });
+    });
+
+    // WebSocket close event handler
+    ws.on('close', function close() {
+        console.log('Client disconnected from WebSocket');
+    });
+});
+
+
+
+// Start the server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));

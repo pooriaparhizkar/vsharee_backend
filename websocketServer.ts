3182c8a52b4b695a5cb456e7886ddecda3f3f9f3
pooriaparhizkar <pooriaparhizkar@yahoo.com>
2024-03-19 01:22:49 +0330

    Add: online members feature

--- websocketServer.ts
+++ websocketServer.ts
@@ -2,10 +2,11 @@ import WebSocket from 'ws';
 import jwt from 'jsonwebtoken';
 import User from './models/User';
 import Group from './models/Group';
+import { UserType } from './interface';
 
 interface WebSocketConnection {
     ws: WebSocket;
-    userId: string;
+    user: any;
     groupId: string;
     pingInterval: NodeJS.Timeout; // Store the ping interval for each connection
 }
@@ -14,6 +15,20 @@ const connections: WebSocketConnection[] = [];
 
 export function initializeWebSocketServer(server: any) {
     const wss = new WebSocket.Server({ server });
+
+    function sendMessageToEveryone(groupId: string, sender: any, data: Object) {
+        connections.filter(connection => connection.groupId === groupId).forEach(
+            connection => {
+                if (connection.ws.readyState === WebSocket.OPEN) {
+                    connection.ws.send(JSON.stringify({
+                        sender: sender,
+                        data
+                    }));
+                }
+            }
+        );
+    }
+
 
     wss.on('connection', function connection(ws, req) {
         if (!req.url || req.url === "") return;
@@ -56,35 +71,53 @@ export function initializeWebSocketServer(server: any) {
                 }, 30000); // Send ping every 30 seconds
 
                 // Store the connection information
-                connections.push({ ws, userId: decoded.userId, groupId, pingInterval });
+                connections.push({ ws, user: sender, groupId, pingInterval });
+                const groupOnlineMembers: any = []
+                connections.filter(connection => connection.groupId === groupId).forEach(
+                    connection => {
+                        if (connection.ws.readyState === WebSocket.OPEN) {
+                            groupOnlineMembers.push(connection.user)
+                        }
+                    }
+                );
+                ws.send(JSON.stringify({
+                    data: {
+                        type: "prev-online",
+                        content: groupOnlineMembers
+                    }
+
+                }));
+                const onlineMessage = {
+                    type: "online",
+                    content: sender
+
+                };
+                sendMessageToEveryone(groupId, null, onlineMessage);
 
                 ws.on('message', function incoming(message) {
                     // Convert message to string if it's not already
                     const stringMessage = typeof message !== 'string' ? message.toString() : message;
-
-                    console.log(JSON.parse(stringMessage).type);
                     if (JSON.parse(stringMessage).type === 'heartbeat') {
+
                         ws.send(JSON.stringify({
                             type: "heartbeat",
+                            content: "heartbeat"
+
                         }))
+
                         return;
                     }
 
+                    sendMessageToEveryone(groupId, sender, stringMessage);
                     // Find all WebSocket connections that belong to other members of the group
-                    connections.filter(connection => connection.groupId === groupId).forEach(
-                        connection => {
-                            if (connection.ws.readyState === WebSocket.OPEN) {
-                                connection.ws.send(JSON.stringify({
-                                    sender: sender,
-                                    content: stringMessage
-                                }));
-                            }
-                        }
-                    );
                 });
 
                 ws.on('close', function close() {
-                    console.log('Client disconnected from WebSocket');
+                    const onlineMessage = {
+                        type: "offline",
+                        content: sender
+                    }
+                    sendMessageToEveryone(groupId, null, onlineMessage);
                     // Clear the ping interval when connection is closed
                     clearInterval(pingInterval);
                     // Remove the connection from the connections array

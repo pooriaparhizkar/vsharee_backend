3875c33a07155c0b40f0fd62dcf29f6f8d0833b9
Pooria Parhizkar <pouorix@gmail.com>
2025-07-13 00:27:55 +0330

    Add: websocket to the project

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger';
 import { authRoutes, groupRoutes, profileRoutes } from './routes';
+import { Server as SocketIOServer } from 'socket.io';
+import http from 'http';
 
 dotenv.config();
 
@@ -25,7 +27,36 @@ app.get('/', (req, res) => {
     res.send('vSharee backend is running!');
 });
 
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+    cors: {
+        origin: '*', // Adjust in production
+        methods: ['GET', 'POST'],
+    },
+});
+io.on('connection', (socket) => {
+    console.log('New socket connection:', socket.id);
+
+    socket.on('joinGroup', ({ groupId, userId }) => {
+        socket.join(groupId);
+        socket.to(groupId).emit('userJoined', { userId });
+    });
+
+    socket.on('sendMessage', ({ groupId, message, user }) => {
+        io.to(groupId).emit('newMessage', { message, user });
+    });
+
+    socket.on('videoControl', ({ groupId, action }) => {
+        io.to(groupId).emit('syncVideo', action); // { type: 'play', time: 120 }
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Socket ${socket.id} disconnected`);
+        // Optionally broadcast 'userLeft'
+    });
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server listening on port 8000`);
 });

11fa1c7cd9176fbf39db9417aab48e11734f12e8
Pooria Parhizkar <pouorix@gmail.com>
2025-08-16 22:46:15 +0330

    Delete: HTTPS local

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,6 @@ import swaggerSpec from './config/swagger';
 import { authRoutes, groupRoutes, profileRoutes } from './routes';
 import { createServer } from 'http';
 import { setupSocket } from './socket';
-import fs from 'fs';
-import path from 'path';
-import https from 'https';
 
 dotenv.config();
 
@@ -28,13 +25,7 @@ app.use('/api/group', groupRoutes);
 
 app.get('/', (req, res) => res.send('vSharee backend is running!'));
 
-// Read SSL certificate and key
-const sslOptions = {
-    key: fs.readFileSync(path.resolve(__dirname, '../certs/localhost-key.pem')),
-    cert: fs.readFileSync(path.resolve(__dirname, '../certs/localhost.pem')),
-};
-
-const server = https.createServer(sslOptions, app);
+const server = createServer(app);
 setupSocket(server); // 👉 Socket.io initialized
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server listening on port 8000`));
